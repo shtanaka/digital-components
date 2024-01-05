2e913320665074500d59d0c5830a843e07176b7e
Gustavo Santiago <egustavo4@gmail.com>
2024-01-05 10:58:10 -0300

    Fetch data from mock api

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 import { addBurgerMenuEvents } from "./src/components/burger";
 import { addDropdownArrow, addDropdownHideShowEvent, getPageDropdowns } from "./src/components/dropdown";
 
@@ -13,9 +12,27 @@ function renderDropdowns() {
   }
 }
 
-function main() {
+async function fetchProducts() {
+  const rawProducts = await fetch("https://6597ee73668d248edf23ba81.mockapi.io/Product");
+  const products = await rawProducts.json();
+  return products;
+}
+
+async function main() {
   addBurgerMenuEvents();
   renderDropdowns();
+  const products = await fetchProducts();
+  const productsContainerEl = document.querySelector('#productsContainer');
+
+  for (const product of products) {
+    const imageEl = document.createElement('img');
+    imageEl.src = product.image;
+    const productNameEl = document.createElement('div');
+    productNameEl.innerHTML = product.name;
+    productsContainerEl.appendChild(imageEl);
+    productsContainerEl.appendChild(productNameEl);
+  }
 }
 
 main();
+
