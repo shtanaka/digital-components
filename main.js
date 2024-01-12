def6451cea5700cb9f8dcf38e613a8205c1a0a4a
Gustavo Santiago <egustavo4@gmail.com>
2024-01-12 11:27:18 -0300

    Add input filters

--- main.js
+++ main.js
@@ -13,25 +13,63 @@ function renderDropdowns() {
 }
 
 async function fetchProducts() {
-  const rawProducts = await fetch("https://6597ee73668d248edf23ba81.mockapi.io/Product");
-  const products = await rawProducts.json();
+  const res = await fetch('https://6597ee73668d248edf23ba81.mockapi.io/Product');
+  const products = await res.json();
   return products;
+}
+
+async function renderProducts(products) {
+  const productsContainer = document.querySelector('#productsContainer');
+  productsContainer.innerHTML = '';
+  for (const product of products) {
+    const div = document.createElement('div');
+    const imageEl = document.createElement('img')
+    const nameEl = document.createElement('div')
+    const priceEl = document.createElement('div')
+
+    imageEl.src = product.image;
+    nameEl.innerHTML = product.name;
+    priceEl.innerHTML = product.price;
+
+    div.appendChild(imageEl);
+    div.appendChild(nameEl);
+    div.appendChild(priceEl);
+
+    productsContainer.appendChild(div);
+  }
+}
+
+function renderSumOfProducts(value) {
+  const sumOfProductsEl = document.querySelector('#sumOfProducts');
+  sumOfProductsEl.innerHTML = `Valor total em estoque: ${value}`
+}
+
+function addInputChangeEvent(input, products) {
+  input.addEventListener('change', () => {
+    renderPage(products, input.value);
+  })
+}
+
+function renderPage(products, filterValue) {
+  const value = Number(filterValue);
+  const filteredProducts = products.filter((product) => Number(product.price) < value);
+  const sumOfValuesOfFilteredProducts = filteredProducts.reduce((acc, item) => {
+    return acc + Number(item.price);
+  }, 0);
+  
+  renderProducts(filteredProducts);
+  renderSumOfProducts(sumOfValuesOfFilteredProducts);
 }
 
 async function main() {
   addBurgerMenuEvents();
   renderDropdowns();
-  const products = await fetchProducts();
-  const productsContainerEl = document.querySelector('#productsContainer');
 
-  for (const product of products) {
-    const imageEl = document.createElement('img');
-    imageEl.src = product.image;
-    const productNameEl = document.createElement('div');
-    productNameEl.innerHTML = product.name;
-    productsContainerEl.appendChild(imageEl);
-    productsContainerEl.appendChild(productNameEl);
-  }
+  const products = await fetchProducts();
+  const inputEl = document.querySelector('#valorAte');
+  const value = Number(inputEl.value);
+  renderPage(products, value);
+  addInputChangeEvent(inputEl, products);
 }
 
 main();
